ec52c047bf886be956982829fb6d8107a6a14e9e
ioangheraszim <gijohny17@gmail.com>
2025-01-25 14:30:38 +0000

    Added functions for all basic math operators - Created add, substract, multiply and divide functions

---
+++ script.js
@@ -0,0 +1,41 @@
+// Variables holding the queries
+let buttonsContainer = document.querySelector(".container_buttons-numbers");
+let display = document.querySelector(".container_display-text");
+let equals = document.querySelector(".container_buttons-numbers--equals");
+let reset = document.querySelector(".container_buttons-numbers--reset");
+
+// Variables
+let displayValue = '0';
+let initialValue = true;
+
+let firstNumber = '';
+let secondNumber = '';
+let currentOperator = '';
+
+// Button array
+const buttons = [
+    '7', '8', '9', 'DEL',
+    '4', '5', '6', '-',
+    '1', '2', '3', '+',
+    '0', '/', '*', '.',
+];
+
+// Addition function
+function add(a, b) {
+    return a + b;
+}
+
+// Subtraction function
+function subtract(a, b) {
+    return a - b;
+}
+
+// Multiplication function
+function multiply(a, b) {
+    return a * b;
+}
+
+// Division function
+function divide(a, b) {
+    return a / b;
+}
